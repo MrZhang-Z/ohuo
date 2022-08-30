e01b53c0cae086ed7eb1f7fdaf46a9102b16cc59
MrZhang-Z <www.mr-zhang@qq.com>
2022-08-30 10:01:41 +0800

    走马灯组件

--- src/components/publicComponents/Carousel.vue
+++ src/components/publicComponents/Carousel.vue
@@ -1,146 +1,238 @@
 <template>
-  <div class="carousel-box">
-    <div class="arrow-box left-arrow" @click="prev_click">
-      <ArrowLeft class="arrow fc-theme-color" />
+  <div class="carousel-box" @mouseover="carousel_mouseover" @mouseout="autoplay_change">
+    <!-- 左箭头 -->
+    <div class="arrow left-arrow fc-theme-color br-2" @click="prev_click" v-show="generalIndex > 1">
+      <ArrowLeft class="arrow-icon" />
     </div>
-    <div class="carousel-inner-box">
-      <div class="carousel-item flex-row" ref="carouselItem">
+    <div class="carousel-item-box" :style="`width:${width};height:${height};`">
+      <div class="carousel-inner-box flex-row" ref="carouselItem">
         <slot></slot>
       </div>
     </div>
-    <div class="arrow-box right-arrow" @click="next_click">
-      <ArrowRight class="arrow fc-theme-color" />
+    <!-- 右箭头 -->
+    <div class="arrow right-arrow fc-theme-color br-2" @click="next_click" v-show="generalIndex > 1">
+      <ArrowRight class="arrow-icon" />
     </div>
-    <div class="indicator-box m-tb-20 flex-row">
-      <div class="indicator m-lr-5 br-2 pointer" :class="{ 'indicator-selected': currentIndex == i }"
-        v-for="i in generalIndex" :key="i"></div>
+    <!-- 指示器 -->
+    <div class="indicator-box flex-row m-tb-10" :class="{ 'inner-indicator-box': indicatorPosition == 'inner' }"
+      v-show="generalIndex > 1">
+      <div class="indicator pointer br-4" :class="{ 'indicator-selected': currentIndex == i }" v-for="i of generalIndex"
+        :key="i" @click="trigger == 'click' ? indicator_click(i) : null"
+        @mouseover="trigger == 'hover' ? indicator_click(i) : null">
+      </div>
     </div>
   </div>
 </template>
-<script lang='ts'>
-import { defineComponent, ref, onMounted } from 'vue';
-// 引入组件
-export default defineComponent({
-  // 走马灯组件
-  name: 'Carousel',
-  setup() {
-    let carouselItem = ref<HTMLElement | null>()
 
-    // 内容总宽度
-    let carouselItemWidth = 0
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    // 走马灯宽度
+    width: {
+      type: String,
+      default: "",
+    },
+    // 走马灯高度
+    height: {
+      type: String,
+      default: "",
+    },
+    // 指示器触发方式 click/hover
+    trigger: {
+      type: String,
+      default: 'click'
+    },
+    // 指示器位置 outside/inner
+    indicatorPosition: {
+      type: String,
+      default: 'outside'
+    },
+    // 自动播放
+    autoplay: {
+      type: Boolean,
+      default: false
+    },
+    // 自动切换间隔
+    interval: {
+      type: Number,
+      default: 3000
+    }
+  },
+  setup(props) {
+    // 总宽
+    const overallWidth = ref(0);
+    // 移动宽度
+    const moveWidth = ref(0);
     // 总页数
-    let generalIndex = ref<number>(0)
+    let generalIndex = ref<number>(0);
     // 当前页
-    let currentIndex = ref<number>(1)
+    let currentIndex = ref<number>(1);
+    // 定时器id
+    let timerId = ref<number>(0)
+
+    const carouselItem = ref<HTMLElement | null>(null);
 
     onMounted(() => {
       if (carouselItem.value) {
-        carouselItemWidth = carouselItem.value.children[0].clientWidth * carouselItem.value.children.length
-        generalIndex.value = (Math.ceil(carouselItemWidth / 1200))
+        overallWidth.value =
+          carouselItem.value?.children[0].clientWidth *
+          carouselItem.value?.children.length;
+        moveWidth.value = carouselItem.value.clientWidth;
+
+        generalIndex.value = Math.ceil(overallWidth.value / moveWidth.value)
       }
-    })
+
+      autoplay_change()
+    });
+
+
+    // 上一页
+    function prev_click() {
+      currentIndex.value--
+      if (carouselItem.value) {
+        if (currentIndex.value < 1) {
+          currentIndex.value = generalIndex.value
+          carouselItem.value.style.transform = `translateX(-${moveWidth.value * (generalIndex.value - 1)}px)`;
+        } else {
+          carouselItem.value.style.transform = `translateX(-${moveWidth.value * (currentIndex.value - 1)}px)`;
+        }
+      }
+    }
+    // 下一页
+    function next_click() {
+      currentIndex.value++
+      if (carouselItem.value) {
+        if (currentIndex.value > generalIndex.value) {
+          carouselItem.value.style.transform = `translateX(0px)`;
+          currentIndex.value = 1
+        } else {
+          carouselItem.value.style.transform = `translateX(-${moveWidth.value * (currentIndex.value - 1)}px)`;
+        }
+      }
+    }
+    // 指示器
+    function indicator_click(index: number) {
+      if (index > currentIndex.value) {
+        currentIndex.value = index - 1
+        next_click()
+      } else {
+        currentIndex.value = index + 1
+        prev_click()
+      }
+    }
+    // 自动切换
+    function autoplay_change(): void {
+      if (props.autoplay) {
+        timerId.value = setInterval(() => {
+          next_click()
+        }, props.interval)
+      }
+    }
+    // 覆盖走马灯
+    function carousel_mouseover(): void {
+      clearInterval(timerId.value)
+    }
 
     return {
       carouselItem,
-      carouselItemWidth,
+      overallWidth,
+      moveWidth,
+      currentIndex,
       generalIndex,
-      currentIndex
+      timerId,
+      prev_click,
+      next_click,
+      indicator_click,
+      carousel_mouseover,
+      autoplay_change
     };
-  },
-  methods: {
-    prev_click() {
-      this.currentIndex -= 1
-
-      if (this.carouselItem) {
-        if (this.currentIndex < 1) {
-          this.currentIndex = this.generalIndex
-          this.carouselItem.style.transform = `translateX(-${1200 * (this.generalIndex - 1)}px)`
-        } else {
-          this.carouselItem.style.transform = `translateX(-${1200 * (this.currentIndex - 1)}px)`
-        }
-      }
-    },
-    next_click() {
-      this.currentIndex += 1
-
-      if (this.carouselItem) {
-        if (this.currentIndex > this.generalIndex) {
-          this.currentIndex = 1
-          this.carouselItem.style.transform = `translateX(0px)`
-        } else {
-          this.carouselItem.style.transform = `translateX(-${1200 * (this.currentIndex - 1)}px)`
-        }
-      }
-    },
   }
 });
 </script>
-<style scoped lang='scss'>
+
+<style lang="scss" scoped>
 .carousel-box {
   width: 100%;
   position: relative;
   overflow: hidden;
 
-  // 箭头
-  .arrow-box {
-    width: 50px;
-    height: 80px;
-    background-color: rgba(0, 0, 0, 0.1);
+  .carousel-item-box {
+    overflow: hidden;
+    margin: 0 auto;
+
+    .carousel-inner-box {
+      height: 100%;
+      width: 100%;
+      transition: all 1s;
+    }
+  }
+
+  .arrow {
     position: absolute;
+    width: 40px;
+    height: 30%;
+    min-height: 40px;
+    max-height: 100px;
+    background-color: rgba(21, 139, 184, 0.1);
+    text-align: center;
     top: 50%;
     transform: translateY(-50%);
+    z-index: 999;
     transition: all 0.5s;
 
-    .arrow {
-      width: 50px;
-      height: 50px;
-      margin-top: 15px;
+    .arrow-icon {
+      width: 30px;
+      top: 50%;
+      left: 50%;
+      position: absolute;
+      transform: translate(-50%, -50%);
     }
   }
 
-  // 左箭头
   .left-arrow {
-    left: -50px;
+    left: -40px;
   }
 
-  // 右箭头
   .right-arrow {
-    right: -50px;
+    right: -40px;
   }
 
-  .carousel-inner-box {
-    width: 1200px;
-    margin: 0 auto;
-    overflow: hidden;
-
-    .carousel-item {
-      transition: all .5s;
-    }
-  }
-
-  // 指示器
   .indicator-box {
     justify-content: center;
 
     .indicator {
       width: 20px;
       height: 4px;
-      background-color: rgba(44, 150, 191, 0.2);
+      background-color: rgba(21, 139, 184, 0.3);
+      margin-right: 20px;
     }
 
     .indicator-selected {
-      background-color: rgba(44, 150, 191, 1);
+      background-color: #158bb8;
     }
+
+    .indicator:last-child {
+      margin: 0;
+    }
+  }
+
+  .inner-indicator-box {
+    position: absolute;
+    bottom: 0px;
+    left: 50%;
+    transform: translateX(-50%);
   }
 }
 
 .carousel-box:hover {
   .left-arrow {
-    left: 0;
+    left: 0px;
   }
 
   .right-arrow {
-    right: 0;
+    right: 0px;
   }
 }
 </style>
---
+++ src/components/publicComponents/CarouselItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="carousel-item-box">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.carousel-item-box {
+  height: 100%;
+  width: 100%;
+  flex-shrink: 0;
+}
+</style>
