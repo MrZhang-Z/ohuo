7d5a37786b6e7351672a9758f8a028892892b541
MrZhang-Z <www.mr-zhang@qq.com>
2022-08-31 11:51:02 +0800

    配置文件

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const port = 8888;
+module.exports = {
+  devServer: {
+    port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {
+      "/": {
+        target: process.env.VUE_APP_URL,
+        changeOrigin: true, // 是否改变域名
+        ws: false
+        // pathRewrite: {
+        //   // 路径重写
+        //   "/api": "" // 这个意思就是以api开头的，定向到哪里, 如果你的后边还有路径的话， 会自动拼接上
+        // }
+      }
+    }
+  }
+}
