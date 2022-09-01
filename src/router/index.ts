8799b28ff59ea17878e8c42c8dff0e3915835ecd
MrZhang-Z <www.mr-zhang@qq.com>
2022-09-01 22:24:55 +0800

    路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,13 +7,20 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/layout/Layout.vue'),
     redirect: '/index',
     children: [
+      // 首页
       {
         path: '/index',
         name: 'home',
         component: () => import('../views/index/Index.vue'),
-      }
+      },
     ]
   },
+  // 登录
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/Login.vue'),
+  } 
 ]
 
 const router = createRouter({
