b80283ce4aa3eea0711ae7f6a667d2c1a0b818d5
MrZhang-Z <www.mr-zhang@qq.com>
2022-09-08 23:04:13 +0800

    添加状态持久化插件

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+// 状态持久化插件
+import piniaPersist from 'pinia-plugin-persist-uni'
 
 // 引入element-plus 图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+const pinia = createPinia()
+pinia.use(piniaPersist)
+
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
